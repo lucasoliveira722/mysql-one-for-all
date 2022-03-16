f96f9e1ea25a4531202ff03cb9cebeb3abdb04e1
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-03-15 21:12:35 -0300

    Requisito 8 completo

---
+++ desafio08.sql
@@ -0,0 +1,7 @@
+SELECT
+  artists.name AS artista,
+  albums.name AS album
+FROM SpotifyClone.artists AS artists
+JOIN SpotifyClone.albums AS albuns ON albums.artist_id = artists.id
+WHERE artists.name = 'Walter Phoenix'
+ORDER BY album;
