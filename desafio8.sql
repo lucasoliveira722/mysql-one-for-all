a3064d145c98de9634e1e2f4b51b1944505b504f
Lucas Oliveira <69327829+lucasoliveira722@users.noreply.github.com>
2022-03-15 22:21:29 -0300

    Correção gramatical

--- desafio8.sql
+++ desafio8.sql
@@ -2,6 +2,6 @@ SELECT
   artists.name AS artista,
   albums.name AS album
 FROM SpotifyClone.artists AS artists
-JOIN SpotifyClone.albums AS albuns ON albums.artist_id = artists.id
+JOIN SpotifyClone.albums AS albums ON albums.artist_id = artists.id
 WHERE artists.name = 'Walter Phoenix'
 ORDER BY album;
